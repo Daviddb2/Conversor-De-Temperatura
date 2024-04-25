6f1b3dd430315f435036c7c04b5ea00da3e62e15
Davi Dias <davi22552@gmail.com>
2024-04-25 19:08:25 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<div>
+    <p> Sua temperatura em Fahrenheit: 
+        <b>
+        <?php
+
+        $celsius = 32;
+        $conversao = ($celsius * 1.8) + 32;
+
+        echo "$conversao";
+
+
+
+
+        ?>
+        </b>
+    </p>
+</div> 
